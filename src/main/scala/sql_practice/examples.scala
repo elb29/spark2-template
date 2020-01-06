a0cda5fae0d78e551821d4693f42d847578e0a76
Etienne Le Bihan <etienne@ubuntu.ubuntu-domain>
2020-01-06 12:08:48 +0100

    exercise 1

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.functions._
 import spark_helpers.SessionBuilder
 
 object examples {
-  def exec1(): Unit ={
+  def exec1(): Unit = {
     val spark = SessionBuilder.buildSession()
     import spark.implicits._
 
@@ -35,4 +35,49 @@ object examples {
 
 
   }
+
+  def exec2(): Unit = {
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val demoDF = spark.read
+      .option("multiline", false)
+      .option("mode", "PERMISSIVE")
+      .json("/home/etienne/Bureau/Sample Data/Spark/data/demographie_par_commune.json")
+    //demoDF.show
+
+    val departement = spark.read
+      .option("multiline", false)
+      .option("mode", "PERMISSIVE")
+      .csv("/home/etienne/Bureau/Sample Data/Spark/data/departements.txt")
+
+
+    demoDF
+      .agg(sum($"population"))
+      .show
+
+    demoDF
+      .groupBy($"departement")
+      .agg(sum($"population"))
+      .orderBy(sum($"population").desc)
+      .show(20)
+
+    demoDF
+      .groupBy($"departement")
+      .agg(sum($"population"))
+      .orderBy(sum($"population").desc)
+      .join(departement.select($"_c0".alias("nomDepartement"),$"_c1"
+        .alias("numDepartement")),$"departement"===$"numDepartement")
+      .select($"numDepartement",$"nomDepartement",$"sum(population)")
+      .show(20)
+
+
+  }
+
+
+
+
 }
+
+
+
